83ef7221ef2976d04748e260a18601bc05b8cdda
Pattanan Laohawiroje <phattanan1999@gmail.com>
2020-04-16 20:49:14 +0700

    Create Login screen

---
+++ StarBung/StarBung/FormLogin.vb
@@ -0,0 +1,33 @@
+﻿Public Class FormLogin
+    Private Sub Login_Click(sender As Object, e As EventArgs) Handles Login.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_Click_1(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub txt_ID_TextChanged(sender As Object, e As EventArgs) Handles txt_ID.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub txt_password_TextChanged(sender As Object, e As EventArgs) Handles txt_password.TextChanged
+
+    End Sub
+
+    Private Sub FormLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Login_Button_Click(sender As Object, e As EventArgs) Handles Login_Button.Click
+
+    End Sub
+End Class
